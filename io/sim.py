4df445044b45eb7dd93ecb11689592a6b96a67e9
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2020-06-05 12:59:56 -0300

    incisos 7 y 8

--- io/sim.py
+++ io/sim.py
@@ -49,7 +49,7 @@ def regr(x, y):
     plt.plot(x, ajust, 'r')
     return m, b, r
 
-# 1.b
+# region 1.b
 # rand = [0.2707, 0.8705, 0.7298, 0.5, 0.3901]
 # b2 = [0.1353, 0.2707, 0.2707, 0.1804, 0.0902]
 # b3 = [59, 166, 268, 278, 142, 87]
@@ -60,8 +60,9 @@ def regr(x, y):
 # print(res)
 # res = sim1(rand, b3, range(6))
 # print(res)
+# endregion
 
-# 2
+# region 2
 # rand = [0.0243, 0.4918, 0.5846, 0.6632, 0.8427,
 #         0.0147, 0.8266, 0.1539, 0.0429, 0.7295]
 # dat = [
@@ -83,9 +84,9 @@ def regr(x, y):
 # print(res.__len__() - res.count(0))
 # suma = sum(res)
 # print(suma)
+# endregion
 
-
-# 4
+# region 4
 # prob = [0.1, 0.2, 0.3, 0.2, 0.1, 0.1]
 # vals = range(1, 7)
 # rand = [0.63, 0.27, 0.15, 0.99, 0.86, 0.71, 0.74, 0.45, 0.11, 0.02, 0.15, 0.14, 0.18, 0.07,
@@ -114,9 +115,9 @@ def regr(x, y):
 # # plt.axis([0, 7, 0, 10])
 # plt.grid()
 # plt.show()
+# endregion
 
-
-# 5
+# region 5
 # rand = [0.6327, 0.9543, 0.1599, 0.6791, 0.8671, 0.1508, 0.7445, 0.3030, 0.1102, 0.1041, 0.1514, 0.2003, 0.1807, 0.0637, 0.1458,
 #         0.5408, 0.6839, 0.4592, 0.3108, 0.8364, 0.1355, 0.5595, 0.4799, 0.7572, 0.4588, 0.4383, 0.5470, 0.8650, 0.9896, 0.9911]
 # media = 5
@@ -148,51 +149,92 @@ def regr(x, y):
 # print(sigma)
 # print(r**2)
 # plt.show()
+# endregion
 
+# region 6
+# dReap = [2, 3, 4, 5, 6, 7, 8]
+# pReap = [5, 13, 17, 27, 23, 10, 5]
+# dCons = [70, 80, 90, 100, 110, 120, 130]
+# pCons = [7, 10, 18, 28, 21, 10, 6]
 
-# 6
-dReap = [2, 3, 4, 5, 6, 7, 8]
-pReap = [5, 13, 17, 27, 23, 10, 5]
-dCons = [70, 80, 90, 100, 110, 120, 130]
-pCons = [7, 10, 18, 28, 21, 10, 6]
+# aux = sum(pReap)
+# pReap[:] = [x/aux for x in pReap]
+# aux = sum(pCons)
+# pCons[:] = [x/aux for x in pCons]
 
-aux = sum(pReap)
-pReap[:] = [x/aux for x in pReap]
-aux = sum(pCons)
-pCons[:] = [x/aux for x in pCons]
+# r1 = []
+# r2 = []
 
-r1 = []
-r2 = []
+# for i in range(30):
+#     r1.append(random.random())
+#     r2.append(random.random())
 
-for i in range(30):
-    r1.append(random.random())
-    r2.append(random.random())
+# simReap = sim1(r1, pReap, dReap)
+# simCons = sim1(r1, pCons, dCons)
+# simR = list(map(lambda d, L: d*L, simCons, simReap))
 
-simReap = sim1(r1, pReap, dReap)
-simCons = sim1(r1, pCons, dCons)
-simR = list(map(lambda d, L: d*L, simCons, simReap))
+# espera = -1
+# stock = 200
+# gastos = 0
 
-espera = -1
-stock = 200
-gastos = 0
+# # ss = statistics.stdev(simReap)*stat.norm.ppf(0.90)
+# ss = 0
+# for i in range(30):
+#     stock -= simCons[i]
+#     if stock > 0:
+#         gastos += stock*0.3
+#     if espera > 0:
+#         espera -= 1
+#     elif espera == 0:
+#         espera = -1
+#         gastos += -1*stock*2
+#         stock += 500
+#     elif stock < simCons[i]*simReap[i] + ss:
+#         espera = simReap[i]
+#         print("Pedido solicitado dia %d", i)
 
-# ss = statistics.stdev(simReap)*stat.norm.ppf(0.90)
-ss = 0
-for i in range(30):
-    stock -= simCons[i]
-    if stock > 0:
-        gastos += stock*0.3
-    if espera > 0:
-        espera -= 1
-    elif espera == 0:
-        espera = -1
-        gastos += -1*stock*2
-        stock += 500
-    elif stock < simCons[i]*simReap[i] + ss:
-        espera = simReap[i]
-        print("Pedido solicitado dia %d", i)
-
-print(gastos)
+# print(gastos)
 # print(simReap)
 # print(simCons)
 # print(sum(simR)/30)
+# endregion
+
+# region 7
+# rand = [0.5029, 0.7333, 0.7818, 0.4541, 0.0727, 0.2290, 0.3246, 0.8961, 0.8025, 0.5754, 0.4769, 0.9083, 0.0278, 0.8062, 0.1894]
+# util = 120
+# gasto = 150
+# ganancia = 0
+# for i in rand:
+#     ganancia += 50*util
+#     extra = int(stat.norm.ppf(i)*2+50)-50
+#     if extra > 0:
+#         ganancia -= extra*gasto
+#     else:
+#         ganancia += extra*util
+# print(ganancia/rand.__len__())
+# endregion
+
+# region 8
+# def expon(x): return stat.expon.ppf(x, 0, 2)    # tiempo entre personas en minutos
+# def norm(x): return stat.norm.ppf(x)*1+4        # minutos tarde
+
+
+# hist = []
+# n = 8
+# for j in range(30):
+#     esperando = 0
+#     espera = 0
+#     for i in range(n):
+#         espera += norm(random.random())
+#         entrePersonas = expon(random.random())
+#         espera -= entrePersonas
+#         while espera > 0:
+#             esperando += 1
+#             entrePersonas = expon(random.random())
+#             espera -= entrePersonas
+#         hist.append(esperando)
+#         esperando = 1
+#         espera += 120/n
+# print(sum(hist)/(30*n))
+
+# endregion

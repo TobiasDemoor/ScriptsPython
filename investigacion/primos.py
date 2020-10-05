760ec3c17c627cb17d650741230a5a82fed77c05
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2020-10-05 18:54:26 -0300

    Update primos.py

--- investigacion/primos.py
+++ investigacion/primos.py
@@ -9,5 +9,20 @@ def sieve(s):
 
 nat = naturales(2)
 s = sieve(nat)
-for i in range(10):
+for i in range(100):
     print(next(s))
+
+def cords(n):
+    maxP = [1,1]
+    pos = [0,0]
+    indice = 0
+    inc = 1
+    for i in range(1,n+1):
+        print(pos)
+        if pos[indice] == maxP[indice]:
+            indice += 1
+            indice %= 2
+            if pos == maxP:
+                maxP[indice] *= -1
+                inc = -1
+        pos[indice] += inc

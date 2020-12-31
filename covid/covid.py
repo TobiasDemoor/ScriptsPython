d16bb110527ce41e4aeaebce6b194a0225dd352e
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2020-12-31 20:29:45 -0300

    Update covid.py

--- covid/covid.py
+++ covid/covid.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+from scipy.stats import norm
+from math import sqrt
 
 # https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19Casos.csv
 
@@ -27,50 +29,10 @@ confirmados: pd.DataFrame = datos.query('clasificacion_resumen == "Confirmado"')
 # plotSeries(ma7ConfirmDiarios.plot())
 # plotSeries(totalesConfirm.plot())
 
-
-# fecha_internacion != None clasificacion contiene "/No activo/" -> ultima_actualizacion - fecha_internacion
-# fecha_internacion != None fallecido == "SI" -> fecha_fallecimiento - fecha_internacion
-def diasInternado(row: pd.Series, date_format: str = '%Y-%m-%d') -> int:
-    parse_date = lambda string : datetime.datetime.strptime(string, date_format)
-    datei = parse_date(row['fecha_internacion'])
-    if row.fallecido == "SI":
-        datef = parse_date(row['fecha_fallecimiento'])
-    else:
-        datef = parse_date(row['ultima_actualizacion'])
-    return (datef - datei).days
-
-
-internados = confirmados.dropna(subset=['fecha_internacion'])
-internados = internados.query(
-    'residencia_provincia_nombre != "SIN ESPECIFICAR" and ' +
-    '(fallecido == "SI" or clasificacion.str.contains("No activo"))',
-    engine='python'
-)
-
-internados["dias_internado"] = internados.apply(diasInternado, axis=1)
-
-# limpiar los datos
-internados = internados.filter(['dias_internado', 'residencia_provincia_nombre'])
-internados = internados[internados.dias_internado > 0]
-
-grouped = internados.groupby('residencia_provincia_nomjbre')
-fig, ax = plt.subplots()
-grouped.boxplot(subplots=False, rot=45, ax = ax)
-fig.subplots_adjust(bottom=0.25, left=0.05, right=0.95, top=1.0)
-plt.show()
-    
-import sys
-sys.exit(0)
-
-# as_index = False
-# financiamiento = group(confirmados, ["fecha_apertura", "origen_financiamiento"])
-# plotSeries(financiamiento)
-# print(financiamiento)
-
 fallecidos = confirmados.query('fallecido == "SI"')
 # print(f"Cantidad de fallecidos: {len(fallecidos)}")
 
-fallDiarios = group(fallecidos)
+# fallDiarios = group(fallecidos)
 # ma7FallDiarios = movingAverage(fallDiarios)
 # totalesFall = fallDiarios.cumsum()
 
@@ -78,3 +40,53 @@ fallDiarios = group(fallecidos)
 # plotSeries(ma7FallDiarios)
 # plotSeries(totalesFall)
 
+
+def diasDeInternacionPorProvincia():
+    global fallecidos
+
+    def diasInternado(row: pd.Series, date_format: str = '%Y-%m-%d') -> int:
+        parse_date = lambda string : datetime.datetime.strptime(string, date_format)
+        datei = parse_date(row['fecha_internacion'])
+        datef = parse_date(row['fecha_fallecimiento'])
+        return (datef - datei).days
+
+    internados = fallecidos.dropna(subset=['fecha_internacion'])
+    internados = internados.query('carga_provincia_nombre != "SIN ESPECIFICAR"')
+
+    internados["dias_internado"] = internados.apply(diasInternado, axis=1)
+
+    # limpiar los datos
+    internados = internados.filter(['dias_internado', 'carga_provincia_nombre'])
+    internados = internados[internados.dias_internado > 0]
+
+    grouped = internados.groupby('carga_provincia_nombre')
+    fig, ax = plt.subplots()
+    grouped.boxplot(subplots=False, rot=45, ax = ax, whis=[5, 95])
+    fig.subplots_adjust(bottom=0.25, left=0.05, right=0.95, top=1.0)
+    plt.show()
+
+
+def tasaMortalidadPorProvincia():
+    global confirmados, fallecidos
+
+    confirmados_provincia = group(confirmados, ['carga_provincia_nombre'])
+    fallecidos_provincia = group(fallecidos, ['carga_provincia_nombre'])
+    tasa_provincia = fallecidos_provincia/confirmados_provincia
+
+    # calculo el sigma de confianza
+    sigma_provincia = []
+    alpha = 0.95
+    z_alpha = norm.ppf(alpha)
+    for index, tasa in enumerate(tasa_provincia):
+        sigma_provincia.append(z_alpha * sqrt((tasa*(1-tasa))/confirmados_provincia[index]))
+
+    fig, ax = plt.subplots()
+    tasa_provincia.plot(kind="bar", ax=ax, label="Muertes/Confirmados")
+    plt.plot(tasa_provincia.index, tasa_provincia.count()*[tasa_provincia.mean()], 'g--', label="Media")
+    plt.plot(tasa_provincia.index, sigma_provincia, 'k-.', label="Confianza")
+    plt.legend()
+    plt.show()
+
+
+# tasaMortalidadPorProvincia()
+# diasDeInternacionPorProvincia()

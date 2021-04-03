aa3fc215be50c3bfde33858a0bf418bb3a15b507
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2021-04-03 20:47:52 -0300

    log10

--- ingeco/costoCapacidad.py
+++ ingeco/costoCapacidad.py
@@ -1,15 +1,18 @@
 import matplotlib.pyplot as plt
-from math import log
+from math import log10
 import scipy.stats as stats
 
 q = [700, 1500, 2500, 10000, 25000]
 p = [115000, 250000, 350000, 950000, 1400000]
 
-x = [log(i) for i in q]
-y = [log(i) for i in p]
+x = [log10(i) for i in q]
+y = [log10(i) for i in p]
 
 plt.plot(x, y, '.')
 m, b, r, p, err = stats.linregress(x, y)
 plt.plot(x, [m*i+b for i in x])
+plt.yscale('log')
+plt.xscale('log')
+plt.grid(True, which='both')
 plt.show()
 print(m, b, r, p, err)
